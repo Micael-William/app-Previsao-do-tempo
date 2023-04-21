58f99a60a69f4f823c325df9b6ddf999a405f022
Micael-William <mw931991@gmail.com>
2023-04-20 22:26:49 -0300

    refatoração do projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,49 @@
-const form = document.querySelector('.formulario')
+const botao = document.getElementById('botao')
+const campoCidade = document.getElementById('cidade')
+const temperaturaMaxima = document.getElementById('temperatura-maxima')
+const temperaturaMinima = document.getElementById('temperatura-minima')
+const logoClima = document.getElementById('logo-temperatura')
+const descricaoCidade = document.getElementById('descricao-cidade')
+const umidadeTempo = document.getElementById('umidade')
+const descricaoTemperatura = document.getElementById('temperatura')
 
-form.addEventListener('submit', (evento) => {
-    evento.preventDefault()
-
-    const cidade = evento.target['busca_cidade'].value
-
-    buscaCidade(cidade)
-
-})
-
-
-const nomeCidade = document.querySelector('.cidade')
-const temperatura = document.querySelector('.temperatura')
-const umidade = document.querySelector('.umidade')
-const descricao = document.querySelector('.descricao')
-const imagemDescricao = document.querySelector('.img')
-const velocidadeVento = document.querySelector('.vento')
-const tempoAtual = document.querySelector('.tempo')
+botao.addEventListener('click', campoBuscaCidade)
 
 
 
-async function buscaCidade(cidade){
+async function campoBuscaCidade () {
+    
+    const cidade = campoCidade.value
+
     const chave = '9c7067bdfe6ec29dd648ff4e22584141'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chave}&lang=pt_br`
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chave}&lang=pt_br`
     const busca = await fetch(url)
     .then(resposta => resposta)
     .then(dados => dados.json())
+    .finally(final => final)
 
-    nomeCidade.innerText = `Tempo em ${busca.name}`
     
-    temperatura.innerText =  Math.floor(` ${busca.main.temp}`) + '°C'
+    const imagem_temperatura = `http://openweathermap.org/img/wn/${busca.weather[0].icon}.png`
+    const descricao_temperatura = `${busca.weather[0].description}`
+    const nome_da_cidade = busca.name
+    const temperatura_minima = ` Temperatura minima ${Math.floor(busca.main.temp_min)} °c`
+    const temperatura_maxima = ` Temperatura máxima ${Math.floor(busca.main.temp_max)} °c`
+    const umidade = ` Umidade ${busca.main.humidity} %`
+    const temperatura = `${busca.main.temp} °c`
 
-    umidade.innerText = `Umidade ${busca.main.humidity}%`
 
-    descricao.innerText = `${busca.weather[0].description}`
-
-    imagemDescricao.src = `https://openweathermap.org/img/wn/${busca.weather[0].icon}.png`
     
+    logoClima.src = imagem_temperatura
+    descricaoTemperatura.innerText = temperatura
+    descricaoCidade.innerText = nome_da_cidade
+    temperaturaMaxima.innerText = temperatura_maxima
+    temperaturaMinima.innerText = temperatura_minima
+    umidadeTempo.innerText = umidade
+    descricaoTemperatura.innerText = descricao_temperatura
+
+    if (cidade) {
+        campoCidade.value = ''
+    }
+
     console.log(busca)
 }
-
-
-
-
